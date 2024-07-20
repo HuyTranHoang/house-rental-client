b2b26d0888c2e05e0b9c21f9599217d32da210d2
HuynhBao <huynhbao0626@gmail.com>
2024-07-21 01:47:31 +0700

    Navbar-Footer

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,7 +1,26 @@
-function Navbar() {
-    return (
-        <div>Navbar</div>
-    )
-}
+import {Button, Typography, Flex} from 'antd';
+import '../assets/css/navbar.css'
+const { Text } = Typography;
 
-export default Navbar
+const Navbar = () => (
+    <>
+        <img
+            src="https://mogi.vn/content/Images/logo.svg"
+            alt="Logo"
+            className={"logo"}
+        />
+        <Flex gap="small" wrap>
+            <Button type={"text"}  href={"#tim-thue"}>Tìm Thuê</Button>
+            <Button type={"text"} href={"#phi-thanh-vien"}>Phí Thành Viên</Button>
+            <Button type={"text"} href={"#du-an"}>Dự Án</Button>
+            <Button type={"text"} href={"#dang-nhap"}>Đăng Nhập</Button>
+            <Button type="default" className={"buttonStyle"} href={"#dang-tin"}>
+                <Text strong className={"textStyle"}>
+                    Đăng Tin
+                </Text>
+            </Button>
+        </Flex>
+    </>
+);
+
+export default Navbar;
