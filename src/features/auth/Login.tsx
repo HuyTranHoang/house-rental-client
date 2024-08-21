e302caceb5f2a06229c7765fd9ce609e3a49bcf5
Huy Trần <nhokpio95@gmail.com>
2024-08-22 02:49:10 +0700

    Chỉnh sửa form login và register

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,5 +1,5 @@
-import { Button, Checkbox, Col, Divider, Flex, Form, FormProps, Input, Row, Spin, Typography } from 'antd'
-import { AntDesignOutlined, FacebookFilled, GoogleCircleFilled, UnlockOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Checkbox, Col, Divider, Flex, Form, FormProps, Input, Row, Space, Spin, Typography } from 'antd'
+import { AntDesignOutlined, FacebookFilled, GoogleCircleFilled, LockOutlined, UserOutlined } from '@ant-design/icons'
 import { toast } from 'sonner'
 import { useSelector } from 'react-redux'
 import { loginFailure, loginRequest, loginSuccess, selectAuth } from './authSlice.ts'
@@ -33,27 +33,27 @@ function Login() {
   const [spinning, setSpinning] = useState(true)
 
   useEffect(() => {
-    let isMounted = true;
+    let isMounted = true
 
     if (isAuthenticated) {
       delay(300).then(() => {
         if (isMounted) {
-          toast.info('Bạn đã đăng nhập rồi!!!');
-          navigate(redirectTo);
+          toast.info('Bạn đã đăng nhập rồi!!!')
+          navigate(redirectTo)
         }
-      });
+      })
     } else {
       delay(300).then(() => {
         if (isMounted) {
-          setSpinning(false);
+          setSpinning(false)
         }
-      });
+      })
     }
 
     return () => {
-      isMounted = false;
-    };
-  }, [isAuthenticated, navigate, redirectTo]);
+      isMounted = false
+    }
+  }, [isAuthenticated, navigate, redirectTo])
 
   const onFinish = async (values: FieldType) => {
     console.log('Success submit:', values)
@@ -85,87 +85,86 @@ function Login() {
   }
 
   return (
-    <Row style={{ textAlign: 'center', margin: '3rem 0' }}>
-      <Col offset={8} md={8} style={{ backgroundColor: 'white', padding: '2rem', borderRadius: '1rem' }}>
-        <img src="/logo.png" alt="logo image" />
-        <Typography.Text style={{ display: 'block' }}>Nền tảng tìm kiếm và cho thuê nhà trọ</Typography.Text>
-        <Divider />
-        <Form
-          name="login"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          initialValues={{ remember: true }}
-          autoComplete="off"
-          labelCol={{ span: 8 }}
-        >
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập tài khoản!'
-              }
-            ]}
+    <Row className="login-page-bg" style={{ textAlign: 'center', margin: '3rem 0' }}>
+      <Col span={24}>
+        <Flex justify="center" align="middle">
+          <Form
+            name="login"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            style={{
+              width: '400px',
+              backgroundColor: 'white',
+              borderRadius: '8px',
+              padding: '32px',
+              margin: '64px 0',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
+            }}
           >
-            <Input placeholder="Tài khoản" prefix={<UserOutlined />} />
-          </Form.Item>
 
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập mật khẩu!'
-              }
-            ]}
-          >
-            <Input.Password placeholder="Mật khẩu" prefix={<UnlockOutlined />} />
-          </Form.Item>
+            <Flex vertical justify="center" align="center" style={{ marginBottom: '24px' }}>
+              <img src="/logo.png" alt="Mogu logo" style={{ width: 140 }} />
+              <Typography.Title level={3} style={{ textAlign: 'center' }}>Đăng nhập</Typography.Title>
+              <Typography.Text type="secondary" className="centered-text" style={{ fontSize: 12 }}>
+                Chào mừng bạn trở lại,
+                vui lòng nhập thông tin
+                tài khoản để tiếp tục.
+              </Typography.Text>
+            </Flex>
 
-          <Flex justify="space-between" align="center">
-            <Form.Item
-              name="remember"
-              valuePropName="checked"
+            <Form.Item<FieldType>
+              name="username"
+              rules={[{ required: true, message: 'Vui lòng nhập tên tài khoản!' }]}
             >
-              <Checkbox>Nhớ đăng nhập</Checkbox>
+              <Input prefix={<UserOutlined />} placeholder="Tài khoản" />
             </Form.Item>
+
+            <Form.Item<FieldType>
+              name="password"
+              rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}
+            >
+              <Input.Password prefix={<LockOutlined />} placeholder="Mật khẩu" />
+            </Form.Item>
+
             <Form.Item>
-              <Link to={ROUTER_NAMES.REQUEST_RESET_PASSWORD}>
+              <Flex justify="space-between" align="center">
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>Ghi nhớ thông tin</Checkbox>
+                </Form.Item>
+                <a href="">Quên mật khẩu?</a>
+              </Flex>
+            </Form.Item>
+
+            <Form.Item>
+              <GradientButton type="primary" htmlType="submit" icon={<AntDesignOutlined />}
+                              loading={isLoading} block>
+                Đăng nhập
+              </GradientButton>
+            </Form.Item>
+
+            <Space>
+              <Typography.Text>
+                Bạn chưa có tài khoản?
+              </Typography.Text>
+              <Link to={ROUTER_NAMES.REGISTER}>
                 <Button type="link">
-                  Quên mật khẩu?
+                  Đăng ký
                 </Button>
               </Link>
+            </Space>
+
+            <Divider>Hoặc đăng nhập với</Divider>
+
+            <Form.Item style={{ marginBottom: 0 }}>
+              <Flex gap="large" justify="center">
+                <FacebookFilled style={{ fontSize: '1.8rem', color: '#2667F9' }} />
+                <GoogleCircleFilled style={{ fontSize: '1.8rem', color: '#DB4437' }} />
+              </Flex>
             </Form.Item>
-          </Flex>
-
-          <Form.Item style={{ marginBottom: '0.6rem' }}>
-            <GradientButton type="primary" htmlType="submit" size="large" icon={<AntDesignOutlined />}
-                            loading={isLoading} block>
-              Đăng nhập
-            </GradientButton>
-          </Form.Item>
-
-          <Form.Item style={{ display: 'inline' }}>
-            <Typography.Text>
-              Bạn chưa có tài khoản?
-            </Typography.Text>
-            <Link to={ROUTER_NAMES.REGISTER}>
-              <Button type="link">
-                Đăng ký
-              </Button>
-            </Link>
-          </Form.Item>
-
-          <Divider>Hoặc đăng nhập với</Divider>
-
-          <Form.Item style={{ marginBottom: 0 }}>
-            <Flex gap="large" justify="center">
-              <FacebookFilled style={{ fontSize: '1.8rem', color: '#2667F9' }} />
-              <GoogleCircleFilled style={{ fontSize: '1.8rem', color: '#DB4437' }} />
-            </Flex>
-          </Form.Item>
-
-        </Form>
+          </Form>
+        </Flex>
       </Col>
     </Row>
   )
--- src/features/auth/Register.tsx
+++ src/features/auth/Register.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Col, Divider, Flex, Form, FormProps, Input, Row, Spin, Typography } from 'antd'
+import { Alert, Button, Col, Flex, Form, FormProps, Input, Row, Space, Spin, Typography } from 'antd'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { AntDesignOutlined, MailOutlined, PhoneOutlined, UnlockOutlined, UserOutlined } from '@ant-design/icons'
 import GradientButton from '@/components/GradientButton.jsx'
@@ -35,27 +35,27 @@ function Register() {
   const [spinning, setSpinning] = useState(true)
 
   useEffect(() => {
-    let isMounted = true;
+    let isMounted = true
 
     if (isAuthenticated) {
       delay(300).then(() => {
         if (isMounted) {
-          toast.info('Bạn đã đăng nhập rồi!!!');
-          navigate(redirectTo);
+          toast.info('Bạn đã đăng nhập rồi!!!')
+          navigate(redirectTo)
         }
-      });
+      })
     } else {
       delay(300).then(() => {
         if (isMounted) {
-          setSpinning(false);
+          setSpinning(false)
         }
-      });
+      })
     }
 
     return () => {
-      isMounted = false;
-    };
-  }, [isAuthenticated, navigate, redirectTo]);
+      isMounted = false
+    }
+  }, [isAuthenticated, navigate, redirectTo])
 
   const onFinish = async (values: FieldType) => {
     console.log('Success submit:', values)
@@ -82,133 +82,147 @@ function Register() {
 
   return (
     <Row style={{ textAlign: 'center', margin: '3rem 0' }}>
-      <Col offset={8} md={8} style={{ backgroundColor: 'white', padding: '2rem', borderRadius: '1rem' }}>
-        <img src="/logo.png" alt="logo image" />
-        <Typography.Text style={{ display: 'block' }}>Nền tảng tìm kiếm và cho thuê nhà trọ</Typography.Text>
-        <Divider />
-        <Form
-          name="register"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          initialValues={{ remember: true }}
-          autoComplete="off"
-          labelCol={{ span: 8 }}
-        >
+      <Col span={24}>
+        <Flex justify="center" align="middle">
+          <Form
+            name="register"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            style={{
+              width: '400px',
+              backgroundColor: 'white',
+              borderRadius: '8px',
+              padding: '32px',
+              margin: '64px 0',
+              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
+            }}
+          >
 
-          {error && <Form.Item>
-            <Alert message={error} type="error" showIcon />
-          </Form.Item>}
+            <Flex vertical justify="center" align="center" style={{ marginBottom: '24px' }}>
+              <img src="/logo.png" alt="Mogu logo" style={{ width: 140 }} />
+              <Typography.Title level={3} style={{ textAlign: 'center' }}>Đăng ký</Typography.Title>
+              <Typography.Text type="secondary" className="centered-text" style={{ fontSize: 12 }}>
+                Đăng ký tài khoản để sử dụng dịch vụ của chúng tôi
+              </Typography.Text>
+            </Flex>
 
-          <Flex gap="small" justify="space-between">
+            {error && <Form.Item>
+              <Alert message={error} type="error" showIcon />
+            </Form.Item>}
+
+            <Flex gap="small" justify="space-between">
+              <Form.Item
+                name="lastName"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng nhập họ!'
+                  }
+                ]}
+              >
+                <Input placeholder="Họ" />
+              </Form.Item>
+              <Form.Item
+                name="firstName"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng nhập tên!'
+                  }
+                ]}
+              >
+                <Input placeholder="Tên" />
+              </Form.Item>
+            </Flex>
             <Form.Item
-              name="lastName"
+              name="username"
               rules={[
                 {
                   required: true,
-                  message: 'Vui lòng nhập họ!'
+                  message: 'Vui lòng nhập tài khoản!'
+                },
+                {
+                  min: 4,
+                  message: 'Tài khoản phải có ít nhất 4 ký tự!'
+                },
+                {
+                  max: 50,
+                  message: 'Tài khoản không được quá 50 ký tự!'
                 }
               ]}
             >
-              <Input placeholder="Họ" />
+              <Input placeholder="Tài khoản" prefix={<UserOutlined />} />
             </Form.Item>
+
             <Form.Item
-              name="firstName"
+              name="password"
               rules={[
                 {
                   required: true,
-                  message: 'Vui lòng nhập tên!'
+                  message: 'Vui lòng nhập mật khẩu!'
+                },
+                {
+                  pattern: new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+                  message: 'Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ cái và số!'
                 }
               ]}
             >
-              <Input placeholder="Tên" />
+              <Input.Password placeholder="Mật khẩu" prefix={<UnlockOutlined />} />
             </Form.Item>
-          </Flex>
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập tài khoản!'
-              },
-              {
-                min: 4,
-                message: 'Tài khoản phải có ít nhất 4 ký tự!'
-              },
-              {
-                max: 50,
-                message: 'Tài khoản không được quá 50 ký tự!'
-              }
-            ]}
-          >
-            <Input placeholder="Tài khoản" prefix={<UserOutlined />} />
-          </Form.Item>
 
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập mật khẩu!'
-              },
-              {
-                pattern: new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
-                message: 'Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ cái và số!'
-              }
-            ]}
-          >
-            <Input.Password placeholder="Mật khẩu" prefix={<UnlockOutlined />} />
-          </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập email!'
+                },
+                {
+                  type: 'email',
+                  message: 'Email không hợp lệ!'
+                }
+              ]}
+            >
+              <Input placeholder="Email" prefix={<MailOutlined />} />
+            </Form.Item>
 
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập email!'
-              },
-              {
-                type: 'email',
-                message: 'Email không hợp lệ!'
-              }
-            ]}
-          >
-            <Input placeholder="Email" prefix={<MailOutlined />} />
-          </Form.Item>
+            <Form.Item
+              name="phoneNumber"
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập số điện thoại!'
+                },
+                {
+                  pattern: new RegExp(/(84|0[3|5|7|8|9])+([0-9]{8})\b/),
+                  message: 'Số điện thoại không hợp lệ!'
+                }
+              ]}
+            >
+              <Input placeholder="Số điện thoại" prefix={<PhoneOutlined />} />
+            </Form.Item>
 
-          <Form.Item
-            name="phoneNumber"
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập số điện thoại!'
-              },
-              {
-                pattern: new RegExp(/(84|0[3|5|7|8|9])+([0-9]{8})\b/),
-                message: 'Số điện thoại không hợp lệ!'
-              }
-            ]}
-          >
-            <Input placeholder="Số điện thoại" prefix={<PhoneOutlined />} />
-          </Form.Item>
+            <Form.Item style={{ marginBottom: '0.6rem' }}>
+              <GradientButton type="primary" htmlType="submit" size="large" icon={<AntDesignOutlined />}
+                              loading={isLoading} block>
+                Đăng ký
+              </GradientButton>
+            </Form.Item>
 
-          <Form.Item style={{ marginBottom: '0.6rem' }}>
-            <GradientButton type="primary" htmlType="submit" size="large" icon={<AntDesignOutlined />}
-                            loading={isLoading} block>
-              Đăng ký
-            </GradientButton>
-          </Form.Item>
-
-          <Form.Item style={{ display: 'inline' }}>
-            <Typography.Text>
-              Bạn đã có tài khoản?
-            </Typography.Text>
-            <Link to={ROUTER_NAMES.LOGIN}>
-              <Button type="link">
-                Đăng nhập
-              </Button>
-            </Link>
-          </Form.Item>
-        </Form>
+            <Space>
+              <Typography.Text>
+                Bạn đã có tài khoản?
+              </Typography.Text>
+              <Link to={ROUTER_NAMES.LOGIN}>
+                <Button type="link">
+                  Đăng nhập
+                </Button>
+              </Link>
+            </Space>
+          </Form>
+        </Flex>
       </Col>
     </Row>
   )
