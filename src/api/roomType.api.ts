1fa9d2c517a7bfa230ac26be85fb05c08d6817b9
Huy Trần <nhokpio95@gmail.com>
2024-08-24 16:49:38 +0700

    Sửa lỗi typescript

--- src/api/roomType.api.ts
+++ src/api/roomType.api.ts
@@ -1,10 +1,10 @@
 import { toast } from 'sonner'
 import axiosInstance from '@/inteceptor/axiosInstance.ts'
-import { District } from '@/models/district.type.ts'
+import { RoomType } from '@/models/roomType.type.ts'
 
 export const fetchAllRoomTypes = async () => {
   try {
-    const response = await axiosInstance.get<District[]>('/api/room-type/all')
+    const response = await axiosInstance.get<RoomType[]>('/api/room-type/all')
     return response.data
   } catch (error) {
     toast.error('Không thể lấy dữ liệu loại phòng')
