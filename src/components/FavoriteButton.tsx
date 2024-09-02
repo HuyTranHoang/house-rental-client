546d679e5cfdceb6b8f8282f9cc2d99f07a22968
Huy Trần <nhokpio95@gmail.com>
2024-09-02 12:04:40 +0700

    Xóa useEffect FavoriteButton

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,7 +1,7 @@
 import { HeartFilled, HeartOutlined } from '@ant-design/icons'
 import { Tooltip } from 'antd'
 import { clsx } from 'clsx/lite'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 interface FavoriteButtonProps {
   isFavorite: boolean | undefined
@@ -9,23 +9,9 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton = ({ isFavorite, onClick }: FavoriteButtonProps) => {
-  const [isAnimating, setIsAnimating] = useState(false)
-
-  useEffect(() => {
-    if (isAnimating) {
-      const timer = setTimeout(() => setIsAnimating(false), 300)
-      return () => clearTimeout(timer)
-    }
-  }, [isAnimating])
-
-  const handleClick = (e: React.MouseEvent) => {
-    setIsAnimating(true)
-    onClick(e)
-  }
-
   return (
     <div
-      onClick={handleClick}
+      onClick={onClick}
       className={clsx(
         'relative flex cursor-pointer items-center justify-center text-2xl transition ease-in-out hover:scale-110 hover:text-red-400 active:scale-95',
         isFavorite && 'text-red-500'
