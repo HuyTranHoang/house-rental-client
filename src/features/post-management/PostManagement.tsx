4ee6b12267113ed1e365a4cab4d3e915fd4cba37
levuong14112000 <levuong2000gl@gmail.com>
2024-10-08 23:49:40 +0700

    nhin tam duoc

--- src/features/post-management/PostManagement.tsx
+++ src/features/post-management/PostManagement.tsx
@@ -5,7 +5,7 @@ import useAuthStore from '@/store/authStore.ts'
 import { PropertyDataSource, PropertyStatus } from '@/types/property.type.ts'
 import Container from '@/ui/Container'
 import { CheckCircleOutlined, CloseSquareOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
-import { Card, Divider, Flex, Input, TableProps, Tabs, TabsProps, Typography } from 'antd'
+import { Card, Col, Divider, Flex, Input, Row, TableProps, Tabs, TabsProps, Typography } from 'antd'
 import { useState } from 'react'
 
 const items: TabsProps['items'] = [
@@ -65,48 +65,52 @@ function PostManagement() {
   }
 
   return (
-    <Container>
-      <Card
-        title={
-          <Flex align='center' gap={12}>
-            <Typography.Title level={4} className='m-0'>
-              Quản lý bài đăng
-            </Typography.Title>
-            <Divider type='vertical' className='h-6' />
-            <Input.Search
-              placeholder='Tìm kiếm bài đăng, giá, địa chỉ..'
-              allowClear
-              enterButton
-              onSearch={(value) => setSearch(value)}
-              className='w-96'
-            />
-          </Flex>
-        }
-        className='mb-10 mt-12'
-      >
-        <Tabs
-          defaultActiveKey='PENDING'
-          items={items}
-          onChange={(key) => {
-            setStatus(key as PropertyStatus)
-            setPageNumber(1)
-          }}
-        />
+    <Container> 
+      <Row gutter={24}>
+        <Col xs={24} md={24}>
+          <Card
+              title={
+                  <Flex align='center' gap={12}>
+                    <Typography.Title level={4} className='m-0'>
+                      Quản lý bài đăng
+                    </Typography.Title>
+                    <Divider type='vertical' className='h-6' />
+                    <Input.Search
+                      placeholder='Tìm kiếm bài đăng, giá, địa chỉ..'
+                      allowClear
+                      enterButton
+                      onSearch={(value) => setSearch(value)}
+                      className='w-96'
+                    />
+                  </Flex>
+                }
+                className='mb-10 mt-12'
+              >
+                <Tabs
+                  defaultActiveKey='PENDING'
+                  items={items}
+                  onChange={(key) => {
+                    setStatus(key as PropertyStatus)
+                    setPageNumber(1)
+                  }}
+                />
 
-        <PostManagementTable
-          dataSource={dataSource}
-          isLoading={isLoading}
-          handleTableChange={handleTableChange}
-          paginationProps={{
-            total: data?.pageInfo.totalElements,
-            pageSize: pageSize,
-            current: pageNumber,
-            showTotal: (total, range) => `${range[0]}-${range[1]} trong ${total} bất động sản`,
-            onShowSizeChange: (_, size) => setPageSize(size),
-            onChange: (page) => setPageNumber(page)
-          }}
-        />
-      </Card>
+                <PostManagementTable
+                  dataSource={dataSource}
+                  isLoading={isLoading}
+                  handleTableChange={handleTableChange}
+                  paginationProps={{
+                    total: data?.pageInfo.totalElements,
+                    pageSize: pageSize,
+                    current: pageNumber,
+                    showTotal: (total, range) => `${range[0]}-${range[1]} trong ${total} bất động sản`,
+                    onShowSizeChange: (_, size) => setPageSize(size),
+                    onChange: (page) => setPageNumber(page)
+                  }}
+                />
+          </Card>
+        </Col>
+      </Row>
     </Container>
   )
 }
