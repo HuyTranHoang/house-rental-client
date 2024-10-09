3c4dbaa3e67a44bed6d051bedba19ef0765fa64f
Huy Trần <nhokpio95@gmail.com>
2024-10-10 01:54:02 +0700

    i18n support for about page

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
+import en_about from '@/utils/i18n/locales/en/about.json'
 import en_common from '@/utils/i18n/locales/en/common.json'
 import en_contact from '@/utils/i18n/locales/en/contact.json'
 import en_membership from '@/utils/i18n/locales/en/membership.json'
@@ -9,6 +10,7 @@ import en_profile from '@/utils/i18n/locales/en/profile.json'
 import en_propertyDetail from '@/utils/i18n/locales/en/propertyDetail.json'
 import en_recharge from '@/utils/i18n/locales/en/recharge.json'
 
+import vi_about from '@/utils/i18n/locales/vi/about.json'
 import vi_common from '@/utils/i18n/locales/vi/common.json'
 import vi_contact from '@/utils/i18n/locales/vi/contact.json'
 import vi_membership from '@/utils/i18n/locales/vi/membership.json'
@@ -26,7 +28,8 @@ export const resources = {
     contact: en_contact,
     membership: en_membership,
     recharge: en_recharge,
-    profile: en_profile
+    profile: en_profile,
+    about: en_about
   },
   vi: {
     common: vi_common,
@@ -35,7 +38,8 @@ export const resources = {
     contact: vi_contact,
     membership: vi_membership,
     recharge: vi_recharge,
-    profile: vi_profile
+    profile: vi_profile,
+    about: vi_about
   }
 } as const
 
@@ -44,7 +48,7 @@ i18n.use(initReactI18next).init({
   defaultNS,
   lng: localStorage.getItem('i18n') || 'en',
   fallbackLng: ['en', 'vi'],
-  ns: ['common', 'postProperty', 'contact', 'membership', 'recharge', 'profile', 'propertyDetail'],
+  ns: ['common', 'postProperty', 'contact', 'membership', 'recharge', 'profile', 'propertyDetail', 'about'],
   interpolation: {
     escapeValue: false
   }
