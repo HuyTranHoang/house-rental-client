b5887539f48c0a9ec59ed18e8bed8f8daa3f4de0
Huy Trần <nhokpio95@gmail.com>
2024-10-11 03:14:06 +0700

    Cập nhật params cho getAllAdvertisements

--- src/api/advertisement.api.ts
+++ src/api/advertisement.api.ts
@@ -1,14 +1,22 @@
-import axiosInstance from "@/inteceptor/axiosInstance"
-import { Advertisement } from "@/types/advertisement.type"
+import axiosInstance from '@/inteceptor/axiosInstance'
+import { Advertisement } from '@/types/advertisement.type'
 
 export const getAllAdvertisements = async () => {
-    try {
-        const response = await axiosInstance.get<Advertisement[]>('/api/advertisements')
-        if (response.status === 200 ) {
-            return response.data
-        }
-    } catch (error) {
-        console.error(error)
-        throw new Error('Lấy danh sách quảng cáo thất bại')
+  try {
+
+    const params = {
+      isActived: "true"
     }
+
+    const response = await axiosInstance.get<Advertisement[]>('/api/advertisements', {
+      params
+    })
+
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch (error) {
+    console.error(error)
+    throw new Error('Lấy danh sách quảng cáo thất bại')
+  }
 }
